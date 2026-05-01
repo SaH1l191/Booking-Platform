cc4fcb700ac4a5db077d5b5fd614d88cc1342ad0
SaH1l191 <haldankarsahil98@gmail.com>
2026-05-01 12:40:32 +0530

    cleanup: remove dist, generated, node_modules from tracking

--- BookingService/dist/repositories/booking.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createBooking = createBooking;
-const prisma_1 = require("../config/prisma");
-async function createBooking(data) {
-    return prisma_1.prisma.booking.create({ data });
-}
--- BookingService/dist/src/utils/generateIdempotencyKey.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateIdempotencyKey = generateIdempotencyKey;
-const uuid_1 = require("uuid");
-async function generateIdempotencyKey() {
-    return (0, uuid_1.v4)();
-}
